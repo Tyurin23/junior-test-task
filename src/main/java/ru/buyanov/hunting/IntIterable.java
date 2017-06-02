5916f80abec7773badffead5084d8839f70d3947
fruit <fruit22@mail.ru>
2017-06-02 20:20:00 +0300

    imlimentation for methods

--- src/main/java/ru/buyanov/hunting/IntIterable.java
+++ src/main/java/ru/buyanov/hunting/IntIterable.java
@@ -18,15 +18,14 @@ public class IntIterable implements Iterable<Integer> {
     }
 
     private class IntIterator implements Iterator<Integer> {
+        int cursor;
 
         public boolean hasNext() {
-            //TODO: You task is implement this method
-            return false;
+            return cursor != backed.length;
         }
 
         public Integer next() {
-            //TODO: You task is implement this method
-            return null;
+            return backed[cursor++];
         }
 
         public void remove() {
