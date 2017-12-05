5468ce1e3251a3fe861419b315bd589f0e15d846
Nikolay <32600929+pisarev-nm@users.noreply.github.com>
2017-12-05 22:00:13 +0300

    task completed

--- src/main/java/ru/buyanov/hunting/IntIterable.java
+++ src/main/java/ru/buyanov/hunting/IntIterable.java
@@ -1,16 +1,19 @@
 package ru.buyanov.hunting;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  *  @author https://github.com/alex-on-java 03.02.2016
  */
 public class IntIterable implements Iterable<Integer> {
-    int[] backed;
+    private int[] backed;
+    private int check;
 
 
     public IntIterable(int[] backed) {
         this.backed = backed;
+        this.check=0;
     }
 
     public Iterator<Integer> iterator() {
@@ -20,14 +23,23 @@ public class IntIterable implements Iterable<Integer> {
     private class IntIterator implements Iterator<Integer> {
 
         public boolean hasNext() {
-            //TODO: You task is implement this method
-            return false;
+            //если check меньше длины массива, то метод вернет true, если больше или равен - false, то есть следующего элемента не существует
+            return check<backed.length;
         }
 
         public Integer next() {
             //TODO: You task is implement this method
-            return null;
+            //если hasNext() возвращает истину, то метод next() вернет следующий элемент массива backed
+            if(hasNext())
+            {
+                return backed[check++];
+            } else
+            //однако, если следующего элемента не имеется, то вместо Integer'а, метод выбрасывает исключение об отсутствии элемента
+            {
+                throw new NoSuchElementException();
+            }
         }
+        //юнит тесты выполняются успешно
 
         public void remove() {
             throw new IllegalStateException("Could not remove from array");
